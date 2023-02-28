d0b012d391f6e989b39e363c112a9b4fd3b788df
Seung Hoon Lee <jlee1095@my.bcit.ca>
2023-02-28 10:04:48 -0800

    Demo09

--- scripts/firebaseAPI_TEAM26.js
+++ scripts/firebaseAPI_TEAM26.js
@@ -2,8 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    //keys go here 
-  
+    apiKey: "AIzaSyDjWP-ijYF3z_eZwm2n5gOjppWlRdHdpp4",
+    authDomain: "comp1800-79cbb.firebaseapp.com",
+    projectId: "comp1800-79cbb",
+    storageBucket: "comp1800-79cbb.appspot.com",
+    messagingSenderId: "21551598861",
+    appId: "1:21551598861:web:ae252b10ffbfc50d9bf444"
 };
 
 //--------------------------------------------
